ada10a9300bfed80cc1b30557dff73539e7400c3
SommersetMoem <nnekit10@mail.ru>
2024-04-27 11:50:45 +0300

    Refactoring api test

---
+++ src/test/java/tests/ui/SeleniumTests.java
@@ -0,0 +1,14 @@
+package tests.ui;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTests {
+    @Test
+    public void simpleTest() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.
+    }
+}
