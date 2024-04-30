2659f5ba639d4119baf44cbf79a06fdc7f1763a0
Никита Василевский <nnekit10@yandex>
2024-04-30 12:22:11 +0300

    First ui test

--- src/test/java/tests/ui/SeleniumTests.java
+++ src/test/java/tests/ui/SeleniumTests.java
@@ -1,14 +1,33 @@
 package tests.ui;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class SeleniumTests {
+    private WebDriver webDriver;
+
+    @BeforeEach
+    public void setup() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        webDriver.manage().window().setSize(new Dimension(1920, 1080));
+    }
+
     @Test
     public void simpleTest() {
-        WebDriver driver = new ChromeDriver();
+        String expectedTitle = "Олег Пендрак - Инженер по автоматизации тестирования QA Automation";
 
-        driver.
+        webDriver.get("https://threadqa.ru/");
+        String actualTitle = webDriver.getTitle();
+
+        Assertions.assertEquals(expectedTitle, actualTitle);
+        webDriver.close();
     }
 }
